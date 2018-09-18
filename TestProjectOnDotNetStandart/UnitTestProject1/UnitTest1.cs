09da9b44e8e7460d78c73feef576aef1b4bdc3ef
Evgen Gavrasienko <42748874+llasapg@users.noreply.github.com>
2018-09-18 19:23:13 +0300

    Add files via upload

---
+++ TestProjectOnDotNetStandart/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using ClassLibrary1;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class BingTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            using (IWebDriver dr = new ChromeDriver(Directory.GetCurrentDirectory()))
+            {
+                var google = new Google(dr);
+                google.FullScreen();
+                Thread.Sleep(2000);
+                google.MoveToGoogle();
+                Thread.Sleep(2000);
+                google.Search(Rozetka.Url);
+                Thread.Sleep(2000);
+                google.Click();
+                Thread.Sleep(2000);
+                var rozetka = Google.MoveNext();
+                Thread.Sleep(2000);
+                rozetka.MoveToRozetka();
+                Thread.Sleep(2000);
+                rozetka.FindLepTop();
+                Thread.Sleep(2000);
+                rozetka.FindApple();
+                Thread.Sleep(2000);
+                rozetka.MoveToAppleLepTop();
+                Thread.Sleep(2000);
+                rozetka.BuyLepTop();
+                Thread.Sleep(2000);
+                //google.MoveNext();
+            }
+        }
+    }
+}
